59b2cbfb8faaf6a676f8655c83cc7936033495fe
Jonathan Doyle <jdoyle@guidwire.com>
2020-04-28 13:45:33 +0100

    Updates

---
+++ precache-manifest.008749687a4bce8a4ffe0a9afe9aa803.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8668084fd8ac105b85dda687a9789fa7",
+    "url": "/GolfScorecard/index.html"
+  },
+  {
+    "revision": "5c04b0ed998c7cd8e2ba",
+    "url": "/GolfScorecard/static/css/main.f5989333.chunk.css"
+  },
+  {
+    "revision": "597aa46b0839e4f24665",
+    "url": "/GolfScorecard/static/js/2.a2ddfcba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GolfScorecard/static/js/2.a2ddfcba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c04b0ed998c7cd8e2ba",
+    "url": "/GolfScorecard/static/js/main.044d1b84.chunk.js"
+  },
+  {
+    "revision": "cbf75f75005c3a5cfb2d",
+    "url": "/GolfScorecard/static/js/runtime-main.62fe7318.js"
+  }
+]);
